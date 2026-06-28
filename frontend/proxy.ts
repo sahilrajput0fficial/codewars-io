3b01c41956cf18b315bc6c1c63b553115727c635
Dev-Aakash-Mishra <aakashmishra2050880@gmail.com>
2026-06-28 19:14:32 +0530

    added the auth and production functionality

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -7,17 +7,15 @@ export async function middleware(request: NextRequest) {
 export async function proxy(request: NextRequest) {
   return await updateSession(request);
 }
-export const BASE_URL = "http://localhost:8000";
+
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  (process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
+    ? "https://codewars-io.vercel.app/_/backend"
+    : "http://localhost:8000");
+
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
